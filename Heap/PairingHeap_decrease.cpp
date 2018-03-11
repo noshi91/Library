31b88433be0020f15738120f10599ab57d9549df
noshi91 <noshita_y@outlook.jp>
2018-03-11 18:50:43 +0900

    Heap

--- Heap/PairingHeap_decrease.cpp
+++ Heap/PairingHeap_decrease.cpp
@@ -4,7 +4,7 @@
 #include <utility>
 /*
 
-tempplate<typename T, bool P = false>
+template<typename T, bool P = false>
 class PairingHeap;
 
 PairingHeapは融合可能なヒープ(優先度付きキュー)です
@@ -34,18 +34,34 @@ PairingHeapは融合可能なヒープ(優先度付きキュー)です
  先頭の要素を削除し、そのconst参照を返します
  時間計算量 償却 O(logN)
 
--push (const T &data)
+-push (T data)->node_t *
  data を要素としてヒープに追加します
+ 追加された要素へのポインタを返します
  時間計算量 O(1)
 
 -empty ()->bool
  ヒープが空かどうかを返します
  時間計算量 O(1)
 
+-size ()->uint32
+ 要素数を返します
+ 時間計算量 O(1)
+
 -meld (ParingHeap<T, P> &other)
  other の持つ要素全てをヒープに追加します
  otherは空になります
  時間計算量 O(1)
+
+-decrease (node_t *x, T data)
+ x のキーの値を data に変更します
+ data は変更前の値以下(P=true ならば以上)である必要があります
+ 時間計算量 O(1)
+
+
+メンバ型
+-node_t
+ 要素を格納しているノードの型
+ push の返り値として与えられ、decrease の際に使用する
 
 
 ※N:要素数
@@ -55,27 +71,35 @@ PairingHeapは融合可能なヒープ(優先度付きキュー)です
 
 template <typename T, bool P = false> class PairingHeap {
   static constexpr std::uint_fast64_t ALLOCSIZE = (std::uint_fast64_t)1 << 10;
+
+public:
   struct node_t {
     T data;
-    node_t *left, *right;
+    node_t *left, *right, *per;
   };
+
+private:
   node_t *root;
+  std::uint_fast32_t size_;
   static node_t *merge(node_t *x, node_t *y) {
     if (P ^ (x->data < y->data))
       std::swap(x, y);
-    x->right = y->left;
+    if (x->right = y->left)
+      y->left->per = x;
     y->left = x;
+    x->per = y;
     return y;
   }
 
 public:
-  PairingHeap() : root(nullptr) {}
+  PairingHeap() : root(nullptr), size_(0) {}
   const T &top() const {
     assert(root);
     return root->data;
   }
   const T &pop() {
     assert(root);
+    --size_;
     const T &ret = root->data;
     node_t *n = nullptr, *x = root->left, *y;
     while (x) {
@@ -100,9 +124,11 @@ public:
       n = n->right;
       root = merge(root, x);
     }
+    root->per = nullptr;
+    root->right = nullptr;
     return ret;
   }
-  void push(const T &data) {
+  const node_t *push(const T &data) {
     static node_t *pool = nullptr;
     static std::uint_fast64_t it = ALLOCSIZE;
     if (it == ALLOCSIZE) {
@@ -111,14 +137,36 @@ public:
     }
     node_t *top = &pool[it++];
     top->data = data;
-    top->left = nullptr;
+    top->left = top->right = top->per = nullptr;
     root = root ? merge(root, top) : top;
+    ++size_;
+    return top;
   }
   bool empty() { return !root; }
+  std::uint_fast32_t size() { return size_; }
   void meld(PairingHeap<T, P> &other) {
+    size_ += other.size_;
+    other.size_ = 0;
     if (other.root) {
       root = root ? merge(root, other.root) : other.root;
       other.root = nullptr;
     }
+  }
+  void decrease(node_t *const x, const T &data) {
+    assert(x);
+    assert(!(P ^ (x->data < data)));
+    if (!x->per) {
+      x->data = data;
+      return;
+    }
+    if (x->right)
+      x->right->per = x->per;
+    if (x->per->right != x)
+      x->per->left = x->right;
+    else
+      x->per->right = x->right;
+    x->per = nullptr;
+    x->right = nullptr;
+    root = merge(root, x);
   }
 };
