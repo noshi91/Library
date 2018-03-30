61334ea76cce8af49bb74a15c4127d2a5fb56fc6
noshi91 <noshita_y@outlook.jp>
2018-03-30 16:23:33 +0900

    ア

--- DisjointSetDataStructure/PotentializedUnionFindTree.cpp
+++ DisjointSetDataStructure/PotentializedUnionFindTree.cpp
@@ -1,51 +1,81 @@
+#include <cassert>
 #include <cstdint>
-#include <utility>
+#include <functional>
+#include <tuple>
 #include <vector>
 
 template <typename Abelian> class PotentializedUnionFindTree {
-  using int32 = std::int_fast32_t;
-  using uint32 = std::uint_fast32_t;
-  std::vector<std::pair<int32, Abelian>> tree;
 
 public:
-  PotentializedUnionFindTree(uint32 size, Abelian e = Abelian())
-      : tree(size, {-1, e}) {}
-  uint32 find(const uint32 x) {
-    if (tree[x].first < 0)
+  using value_type = Abelian;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using size_type = std::uint_fast32_t;
+
+private:
+  using F = std::function<value_type(const_reference, const_reference)>;
+  using G = std::function<value_type(const_reference)>;
+  const F f;
+  const G g;
+  const value_type neutral_;
+  std::vector<std::tuple<bool, size_type, value_type>> tree;
+
+public:
+  PotentializedUnionFindTree(const size_type size,
+                             const_reference neutral = value_type(),
+                             const F &f = std::plus<value_type>(),
+                             const G &g = std::negate<value_type>())
+      : f(f), g(g), neutral_(neutral),
+        tree(size, std::forward_as_tuple(1, 1, neutral_)) {}
+  size_type find(const size_type x) {
+    assert(x < tree.size());
+    if (std::get<0>(tree[x]))
       return x;
-    uint32 t = (uint32)tree[x].first;
-    tree[x].first = (int32)find(t);
-    tree[x].second = tree[t].second + tree[x].second;
-    return (uint32)tree[x].first;
+    const size_type t = std::get<1>(tree[x]);
+    std::get<1>(tree[x]) = find(t);
+    std::get<2>(tree[x]) = f(std::get<2>(tree[t]), std::get<2>(tree[x]));
+    return std::get<1>(tree[x]);
   }
-  Abelian potential(const uint32 x) {
+  value_type potential(const size_type x) {
+    assert(x < tree.size());
     find(x);
-    return tree[x].second;
+    return std::get<2>(tree[x]);
   }
-  Abelian diff(const uint32 x, const uint32 y) {
-    return potential(y) + (-potential(x));
+  value_type diff(const size_type x, const size_type y) {
+    assert(x < tree.size());
+    assert(y < tree.size());
+    return f(potential(y), g(potential(x)));
   }
-  bool unite(uint32 x, uint32 y, Abelian d) {
-    d = d + diff(y, x);
+  bool unite(size_type x, size_type y, value_type d) {
+    assert(x < tree.size());
+    assert(y < tree.size());
+    d = f(d, diff(y, x));
     x = find(x);
     y = find(y);
     if (x == y)
       return false;
-    if (tree[x].first > tree[y].first) {
-      std::swap(x, y);
-      d = -d;
-    }
-    tree[x].first += tree[y].first;
-    tree[y] = std::pair<int32, Abelian>(x, d);
+    if (std::get<1>(tree[x]) < std::get<1>(tree[y]))
+      std::swap(x, y), d = g(d);
+    std::get<1>(tree[x]) += std::get<1>(tree[y]);
+    tree[y] = std::forward_as_tuple(0, x, d);
     return true;
   }
-  bool same(const uint32 x, const uint32 y) { return find(x) == find(y); }
-  uint32 size(const uint32 x) { return (uint32)(-tree[find(x)].first); }
+  bool same(const size_type x, const size_type y) {
+    assert(x < tree.size());
+    assert(y < tree.size());
+    return find(x) == find(y);
+  }
+  size_type size(const size_type x) {
+    assert(x < tree.size());
+    return std::get<1>(tree[find(x)]);
+  }
 };
 
 /*
 
-template <typename Abelian>
+verify:https://beta.atcoder.jp/contests/abc087/submissions/2279390
+
+template <typename Abelian, typename Size = std::uint_fast32_t>
 class PotentializedUnionFindTree;
 
 PotentializedUnionFindTreeはポテンシャルが付いた要素からなる素集合を管理するデータ構造です
@@ -59,44 +89,63 @@ PotentializedUnionFindTreeはポテンシャルが付いた要素からなる素
  単位元 ∃e; e + a = a
  逆元　 ∀a, ∃-a; a + (-a) = e
  以上の条件を満たす代数的構造 (アーベル群)
- 二項演算が operator+, 逆元が operator-(単項) で定義されている必要があります
+
+
+メンバ型
+-value_type
+ 要素の型 (Abelian)
+
+-reference
+ 要素(value_type)への参照型 (value_type &)
+
+-const_reference
+ 要素(value_type)へのconst参照型 (const value_type &)
+
+-size_type
+ 符号なし整数型 (std::uint_fast32_t)
 
 
 メンバ関数
--(constructor) (uint32 size, Abelian e = Abelian())
- 独立した要素を size 個持つ状態で構築します
- e には単位元を与えてください
+-(constructor) (size_type size, const_reference neutral = value_type(),
+                std::function<value_type(const_reference, const_reference)>
+                f = std::plus<value_type>(),
+                std::function<value_type(const_reference)>
+                g = std::negate<value_type>())
+ 独立した要素を size 個持つ状態で、neutral を単位元
+ f を演算、g を逆元を得る関数として構築します
+ 各要素のポテンシャルは単位元で初期化されます
  時間計算量 O(N)
 
--find (uint32 x)->uint32
+-find (size_type x)->size_type
  x の根を返します
  時間計算量 償却 O(α(N))
 
--potential (uint32 x)->Abelian
+-potential (size_type x)->value_type
  x の持つポテンシャルを返します
  時間計算量 償却 O(α(N))
 
--diff (uint32 x, uint32 y)->Abelian
+-diff (size_type x, size_type y)->value_type
  x を基準とした y のポテンシャルを返します
  x と y が異なる集合に属していた場合の動作は保証されません
  時間計算量 償却 O(α(N))
 
--unite (uint32 x, uint32 y, Abelian d)->bool
- x と y がそれぞれ含まれる集合を x を基準とした y のポテンシャルが d
- となるように併合します
- 併合に成功したか、すなわち x と y が違う集合に属していたかを返します
+-unite (size_type x, size_type y, value_type d)->bool
+ x と y がそれぞれ含まれる集合を x を基準とした y のポテンシャルが
+ d となるように併合します
+ 併合に成功したか、すなわち x と y が違う集合に属していたかを真偽値で返します
  時間計算量 償却 O(α(N))
 
--same (uint32 x, uint32 y)->bool
- x と y が同じ集合に属しているかを返します
+-same (size_type x, size_type y)->bool
+ x と y が同じ集合に属しているかを真偽値で返します
  時間計算量 償却 O(α(N))
 
--size (uint32 x)->uint32
+-size (size_type x)->size_type
  x の含まれる集合に含まれる要素数を返します
  時間計算量 償却 O(α(N))
 
 
 ※N:全体の要素数
 ※α():アッカーマン関数の逆関数
+※f(), g() の時間計算量を O(1) と仮定
 
 */
