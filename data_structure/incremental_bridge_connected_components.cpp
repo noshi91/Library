52d74c235f178e50593f447ec3051cb47deef019
noshi91 <noshita_y@outlook.jp>
2020-02-07 00:43:27 +0900

    add incremental_bridge_connected_components

---
+++ data_structure/incremental_bridge_connected_components.cpp
@@ -0,0 +1,79 @@
+#include "data_structure/union_find.cpp"
+
+#include <cassert>
+#include <cstddef>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+class incremental_bridge_connected_components {
+  using size_t = std::size_t;
+
+  union_find cc;
+  union_find bcc;
+  std::vector<size_t> bbf;
+
+  size_t size() const { return bbf.size(); }
+  size_t nil() const { return size(); }
+
+  size_t parent(const size_t v) {
+    if (bbf[v] == nil())
+      return nil();
+    else
+      return bcc.find(bbf[v]);
+  }
+  size_t lca(size_t u, size_t v) {
+    std::unordered_set<size_t> reached;
+    while (true) {
+      if (u != nil()) {
+        if (!reached.insert(u).second)
+          return u;
+        u = parent(u);
+      }
+      std::swap(u, v);
+    }
+  }
+  void condense_path(size_t u, const size_t v) {
+    while (!bcc.same(u, v)) {
+      const size_t next = parent(u);
+      bbf[u] = bbf[v];
+      bcc.unite(u, v);
+      u = next;
+    }
+  }
+  void link(const size_t x, const size_t y) {
+    size_t v = x, prev = y;
+    while (v != nil()) {
+      const size_t next = bbf[v];
+      bbf[v] = prev;
+      prev = v;
+      v = next;
+    }
+  }
+
+public:
+  incremental_bridge_connected_components() = default;
+  explicit incremental_bridge_connected_components(const size_t n)
+      : cc(n), bcc(n), bbf(n, n) {}
+
+  void insert_edge(size_t u, size_t v) {
+    assert(u < size());
+    assert(v < size());
+    u = bcc.find(u);
+    v = bcc.find(v);
+    if (cc.same(u, v)) {
+      const size_t w = lca(u, v);
+      condense_path(u, w);
+      condense_path(v, w);
+    } else {
+      if (cc.size(u) > cc.size(v))
+        std::swap(u, v);
+      link(u, v);
+      cc.unite(u, v);
+    }
+  }
+  size_t find_block(const size_t u) {
+    assert(u < size());
+    return bcc.find(u);
+  }
+};
