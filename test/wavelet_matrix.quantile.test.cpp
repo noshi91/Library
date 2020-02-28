0a2d0fad65a83ae256b7a1daafc675b31c5762c3
noshi91 <noshita_y@outlook.jp>
2020-02-28 13:37:22 +0900

    add test

---
+++ test/wavelet_matrix.quantile.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
+
+#include "data_structure/wavelet_matrix.cpp"
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  int n, q;
+  std::cin >> n >> q;
+
+  const auto wm = [&] {
+    std::vector<int> a(n);
+    for (int &e : a)
+      std::cin >> e;
+    return wavelet_matrix<int>(30, a);
+  }();
+
+  for (int i = 0; i != q; i += 1) {
+    int l, r, k;
+    std::cin >> l >> r >> k;
+    std::cout << wm.quantile(l, r, k) << std::endl;
+  }
+}
